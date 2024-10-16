d05c59fc723d277c044b67d3756491a86dcd7b97
hassanafify1 <hafify@ucsc.edu>
2024-10-15 17:29:43 -0700

    Add files via upload

---
+++ sess1.py
@@ -0,0 +1,12 @@
+def info():
+	name = "Hassan Afify"
+	pronouns = "He/Him"
+	movie = "Fight Club"
+	food = "Pasta Bechamel (Egyptian style lasagna with Bechamel sauce)"
+
+	print(f"Full Name: {name}")
+	print(f"Pronouns: {pronouns}")
+	print(f"My favorite movie is {movie}.")
+	print(f"My favorite dish is {food}.")
+
+info()
